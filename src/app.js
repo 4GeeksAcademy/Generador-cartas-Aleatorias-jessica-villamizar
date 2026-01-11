ab4a651eb0418fed4eb67b55c05fa79c74668c35
1989212 <jessica_villamizar.c@hotmail.com>
2026-01-11 03:22:08 +0000

    mejoras

--- src/app.js
+++ src/app.js
@@ -7,10 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
-const cartasPalos = ["♦", "♥", "♠", "♣"];
-const valoresDeCarta = ["A",2,3,4,5,6,7,8,9,"K","Q","J"];
+  console.log("Generador de Cartas Aleatorias");
+
+
+  const cartasPalos = ["♦", "♥", "♠", "♣"];
+  const valoresDeCarta = ["A",2,3,4,5,6,7,8,9,"K","Q","J"];
+
+  function signoRamdon (lista){
+    let elementoAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[elementoAleatorio]
+  }
+const imagen1 = document.querySelector("#imagen1")
+imagen1_Icon.textContent = signoRandon(cartasPalos)
+
+
+
+}
+
 
 
 
