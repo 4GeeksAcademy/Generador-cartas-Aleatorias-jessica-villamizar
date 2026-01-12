0e8fb965e3ee6b1569a22580ef196559374537b4
1989212 <jessica_villamizar.c@hotmail.com>
2026-01-12 19:08:31 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -17,10 +17,23 @@ window.onload = function() {
     let elementoAleatorio = Math.floor(Math.random() * lista.length);
     return lista[elementoAleatorio]
   }
-const imagen1 = document.querySelector("#imagen1")
-imagen1_Icon.textContent = signoRandon(cartasPalos)
+const imagen1_Icon = document.querySelector("#imagen1_Icon");
+const paloAleatorio = signoRamdon(cartasPalos)
+imagen1_Icon.textContent = paloAleatorio;
 
+const imagen2_Icon = this.document.querySelector("#imagen2_Icon");
+imagen2_Icon.textContent = paloAleatorio;
 
+const number = this.document.querySelector("#number");
+const numberAleatorio = signoRamdon(valoresDeCarta);
+number.textContent = numberAleatorio;
+
+if (paloAleatorio == "♦" || paloAleatorio == "♥"){
+  imagen1_Icon.style.color = "red"
+  imagen2_Icon.style.color = "red"
+}
+
+document.getElementById("button").addEventListener("click", generarCarta);
 
 }
 
